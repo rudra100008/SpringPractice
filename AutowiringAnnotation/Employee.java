8ad2a3ddb078ceb643cbe73406cac32ec6452eff
Ashum Bhuju Shrestha <136584855+rudra100008@users.noreply.github.com>
2024-06-29 18:14:44 +0545

    Used @Autowiring and @Qualifier in this code

--- AutowiringAnnotation/Employee.java
+++ AutowiringAnnotation/Employee.java
@@ -1,19 +1,26 @@
 package com.springCore.SpringCore.AutowiringAnnotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.beans.factory.annotation.Qualifier;
+/*  @Autowired  annotation is used in  constructor,property and setter function  
+ * @Qualifier annotation is used to specify which bean to be called/used like in this address2 bean is called same way you can call address1 also
+ * if you replace it with address2
+ * */
 public class Employee {
   public String name;
   @Autowired
+  @Qualifier("address2")
   public Address address;
   
   public Employee() {
 	  super();
   }
+ 
   public Employee(String name,Address address) {
 	  super();
 	this.name=name;
 	this.address=address;
+	System.out.println("Setters");
 }
 public String getName() {
 	return name;
@@ -24,6 +31,7 @@ public void setName(String name) {
 public Address getAddress() {
 	return address;
 }
+
 public void setAddress(Address address) {
 	this.address = address;
 }

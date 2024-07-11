155f5cd89d015a2b6200f2e3359454a4fc76f28e
Ashum Bhuju Shrestha <136584855+rudra100008@users.noreply.github.com>
2024-07-11 19:11:16 +0545

    using mysql as database and learned hibernate

--- SpringORM/src/main/java/com/SpringORM/dao/StudentDao.java
+++ SpringORM/src/main/java/com/SpringORM/dao/StudentDao.java
@@ -4,10 +4,13 @@ import org.springframework.orm.hibernate5.HibernateTemplate;
 
 import com.SpringORM.entity.Student;
 
+import jakarta.transaction.Transactional;
+
 public class StudentDao {
 	
-
+   
 	private HibernateTemplate hibernateTemplate;
+	 @Transactional
   public  int insert(Student student) {
 	 Integer id=(Integer) this.hibernateTemplate.save(student);
 	  return id;
